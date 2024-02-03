5e84a60873d00425ba49230edf867fd75d082f58
ibrahim alnagar <alnnagaribrahim45@gmail.com>
2024-02-03 09:58:11 +0200

    intilize hive and open notesbox box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/views/edit_note_view.dart';
 import 'package:notesapp/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("notes_box");
   runApp(
     MaterialApp(
       routes: {
